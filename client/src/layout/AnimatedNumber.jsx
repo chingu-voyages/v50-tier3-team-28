ba69ed9f7ff8aca266f647ce5f370ffcc206f4ca
Andy Brockmann <PongRVA@users.noreply.github.com>
2024-07-15 11:04:49 -0400

    animate numbers with spring, add Blinker font

---
+++ client/src/layout/AnimatedNumber.jsx
@@ -0,0 +1,11 @@
+import { animated, useSpring } from "react-spring";
+
+export default function AnimatedNumber({ n }) {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 200,
+    config: { mass: 1, tension: 20, friction: 5 },
+  });
+  return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+}
