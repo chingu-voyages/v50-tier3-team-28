e3c0a3a0b1e793f1650a1a13be376c8cd351e9dd
Valeriusdev <kakao757@gmail.com>
2024-07-24 01:14:01 -0700

    display markers on the map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,19 +1,24 @@
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer } from "react-leaflet"
-
+import { MapContainer, TileLayer, Marker } from "react-leaflet"
+import mapData from "./mapData";
 
 export default function Map() {
     return (
         <MapContainer 
-            className="leaflet-container h-screen"
+            className="leaflet-container w-[150rem] h-[25rem]"
             center={[0, 0]}
             zoom={2}
-            scrollWheelZoom={true}
+            // scrollWheelZoom={true}
             >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"           
             />
+            {mapData.map(marker => (
+                <Marker position={[marker.coordinates.latitude, marker.coordinates.longitude]}>
+                </Marker>
+            ))
+            }
         </MapContainer>
     );
 }
