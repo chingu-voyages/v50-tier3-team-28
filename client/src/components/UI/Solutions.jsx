977bc87dc45e53cb2779d7de5cbcb464701d0ed1
ss-deep <deepalisangole@gmail.com>
2024-07-28 17:34:39 -0500

    data for solutions page

--- client/src/components/UI/Solutions.jsx
+++ client/src/components/UI/Solutions.jsx
@@ -5,6 +5,7 @@ function Solutions() {
          h-screen bg-footerTextColor'>
       <h2 className='text-white text-3xl'>This is the Solutions Page</h2>
       <h3 className='text-white text-5xl'> Coming soon</h3>
+      
     </div>
   );
 }
