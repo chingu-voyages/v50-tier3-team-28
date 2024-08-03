8d3ecd07aa923115544e8358d014612b825746df
Valeriusdev <kakao757@gmail.com>
2024-08-03 03:52:25 -0700

    update DetailsSummary component styles
    Co-authored-by: KseniiaRiabova <KseniiaRiabova@users.noreply.github.com>

--- client/src/components/UI/DetailsSummary.jsx
+++ client/src/components/UI/DetailsSummary.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export const DetailsSummary = ({ title, description, ...attrs }) => {
   return (
-    <details {...attrs} className="pt-4 pl-4">
+    <details {...attrs}>
       <summary className="list-none cursor-pointer">{title}</summary>
       <p>{description}</p>
     </details>
