7f58c6417535a581efc62a14fcb6f27727555eb7
Valeriusdev <kakao757@gmail.com>
2024-08-09 03:10:48 -0700

    update ProblemContainer component
    Co-authored-by: KseniiaRiabova <KseniiaRiabova@users.noreply.github.com>

--- client/src/components/UI/ProblemContainer.jsx
+++ client/src/components/UI/ProblemContainer.jsx
@@ -11,14 +11,15 @@ export const ProblemContainter = () => {
       </section>
 
       <section className="flex flex-col justify-between gap-4 md:flex-row">
+
+        <section className="md:w-1/2">
+          <Graph />
+        </section>
+
         <section className="bg-[#D8E5C3] dark:bg-[#3D4D22] h-64 md:w-1/2 md:h-auto rounded-xl">
           <DetailsSummary className={customStyles} title="Pesticides" description="The use of pesticides can kill bees outright or impair their ability to navigate and reproduce leading to a decline in bee populations." />
           <DetailsSummary className={customStyles} title="Climate Change" description="The use of pesticides can kill bees outright or impair their ability to navigate and reproduce leading to a decline in bee populations." />
           <DetailsSummary className={customStyles} title="Habitat Destruction" description="The use of pesticides can kill bees outright or impair their ability to navigate and reproduce leading to a decline in bee populations." />
-        </section>
-
-        <section className="md:w-1/2">
-          <Graph />
         </section>
       </section>
     </section>
