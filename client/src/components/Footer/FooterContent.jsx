b8e4c164283b68a3ced8cf5ca1c1e7616613a2f6
Andy Brockmann <PongRVA@users.noreply.github.com>
2024-09-21 22:23:04 -0400

    center beekeeper image and adjust breakpoint for tech stack logos

--- client/src/components/Footer/FooterContent.jsx
+++ client/src/components/Footer/FooterContent.jsx
@@ -5,7 +5,7 @@ import FooterResources from "./FooterResources";
 
 function FooterContent() {
   return (
-    <div className="flex flex-col md:flex-row justify-between w-full space-y-4 md:space-y-0 md:space-x-10 text-footerBoxColor pt-8">
+    <div className="flex flex-col md:flex-row justify-between w-full space-y-4 md:space-y-0 md:space-x-4 lg:space-x-8 text-footerBoxColor pt-2">
       <FooterUserProfiles profiles={profiles} />
 
       <FooterTechStack />
