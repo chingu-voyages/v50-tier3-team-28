2879d7d5710f1294b3f45a84af92bd332b844c16
r1g023 <limon1285@gmail.com>
2024-10-02 16:40:07 -0500

    fix merge conflicts and update requestTableColumns component to match the requestTable

--- client/src/components/UserProfile/RequestTable.jsx
+++ client/src/components/UserProfile/RequestTable.jsx
@@ -1,86 +1,108 @@
-import DataTable from 'react-data-table-component';
+import DataTable from "react-data-table-component";
 // import tempRequestdata  from './tempRequestdata';
-import PropTypes from 'prop-types';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import AcceptRequestModal from '../UI/AcceptRequestModal';
+import PropTypes from "prop-types";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import AcceptRequestModal from "./AcceptRequestModal";
 
-import columns from './requestTableColumns';
+import columns from "./requestTableColumns";
 
 const tableCustomStyles = {
-	headRow: {
-		style: {
-			backgroundColor: '#F8F8F8',
-		},
-	},
+  headRow: {
+    style: {
+      backgroundColor: "#F8F8F8",
+    },
+  },
 };
 
 export const RequestComponent = ({ fixedHeader, fixedHeaderScrollHeight }) => {
-	const { getAccessTokenSilently } = useAuth0();
-	const [requestData, setRequestData] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [showModal, setShowModal] = useState(false);
-	const [selectedRequest, setSelectedRequest] = useState(null);
+  const { getAccessTokenSilently, user } = useAuth0();
+  const [requestData, setRequestData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedRequest, setSelectedRequest] = useState(null);
 
-	const isDevelopment = process.env.NODE_ENV === 'development';
-	const apiUrl = isDevelopment
-		? 'http://localhost:3003/api'
-		: 'https://be-v50-tier3-team-28.onrender.com/api';
+  // const isDevelopment = process.env.NODE_ENV === 'development';
+	// const apiUrl = isDevelopment
+	// 	? 'http://localhost:3003/api'
+	// 	: 'https://be-v50-tier3-team-28.onrender.com/api';
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			try {
-				const accessToken = await getAccessTokenSilently();
-				const response = await axios.get(`${apiUrl}/requests`, {
-					headers: {
-						Authorization: `Bearer ${accessToken}`,
-					},
-				});
-				const data = response.data.requests;
-				setRequestData(data);
-				setLoading(false);
-			} catch (e) {
-				console.error(e);
-				setLoading(false);
-			}
-		};
-		fetchUserData();
-	}, [getAccessTokenSilently]);
 
-	const handleDetailsClick = (row) => {
-		setSelectedRequest(row);
-		setShowModal(true);
-	};
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently();
+        const response = await axios.get("http://localhost:3003/api/requests", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const data = response.data.requests;
+        setRequestData(data);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, [getAccessTokenSilently]);
 
-	const handleModalClose = () => {
-		setShowModal(false);
-		setSelectedRequest(null);
-	};
+  const handleDetailsClick = (row) => {
+    if (!row) return;
+    const requestWithAcceptanceState = {
+      ...row,
+      isAccepted: row.isAccepted ?? null,
+    };
+    setSelectedRequest(requestWithAcceptanceState);
+    setShowModal(true);
+  };
 
-	return (
-		<>
-			<DataTable
-				columns={columns({ handleDetailsClick, selectedRequest })}
-				data={requestData}
-				fixedHeader={fixedHeader}
-				fixedHeaderScrollHeight={fixedHeaderScrollHeight}
-				customStyles={tableCustomStyles}
-				pagination
-				progressPending={loading}
-			/>
+  const handleModalClose = () => {
+    setShowModal(false);
+    setSelectedRequest(null);
+  };
 
-			{showModal && (
-				<AcceptRequestModal
-					request={selectedRequest}
-					onClose={handleModalClose}
-				/>
-			)}
-		</>
-	);
+  const handleRequestAcceptance = (id) => {
+    setRequestData((prevData) =>
+      prevData.map((request) =>
+        request.id === id
+          ? { ...request, isAccepted: true, beekeeper: user.email }
+          : request
+      )
+    );
+  };
+
+  return (
+    <>
+      <DataTable
+        columns={columns({
+          handleDetailsClick,
+          selectedRequest,
+          user,
+        })}
+        data={requestData}
+        fixedHeader={fixedHeader}
+        fixedHeaderScrollHeight={fixedHeaderScrollHeight}
+        customStyles={tableCustomStyles}
+        pagination
+        progressPending={loading}
+      />
+
+      {showModal && selectedRequest && (
+        <AcceptRequestModal
+          key={selectedRequest._id}
+          request={selectedRequest}
+          onClose={handleModalClose}
+          onAccept={() => handleRequestAcceptance(selectedRequest.id)}
+        />
+      )}
+    </>
+  );
 };
 
 RequestComponent.propTypes = {
-	fixedHeader: PropTypes.bool,
-	fixedHeaderScrollHeight: PropTypes.string,
+  fixedHeader: PropTypes.bool,
+  fixedHeaderScrollHeight: PropTypes.string,
 };
--- client/src/components/UserProfile/requestTableColumns.jsx
+++ client/src/components/UserProfile/requestTableColumns.jsx
@@ -1,75 +1,80 @@
-import { Button } from '../UI/Button';
+import { Button } from "../UI/Button";
+import PropTypes from "prop-types";
 
-export default function requestColumns({
-	handleDetailsClick,
-	selectedRequest,
-}) {
-	return [
-		{
-			id: 'title',
-			name: 'Title',
-			selector: (row) => row.title,
-		},
-		{
-			id: 'status',
-			name: 'Status',
-			selector: (row) => (row.isActive ? 'Active' : 'Inactive'),
-			sortable: true,
-		},
-		{
-			id: 'city',
-			name: 'City',
-			selector: (row) => row.location.city,
-			sortable: true,
-		},
-		{
-			id: 'country',
-			name: 'Country',
-			selector: (row) => row.location.country,
-			sortable: true,
-		},
-		{
-			id: 'date',
-			name: 'Date',
-			selector: (row) => row.createdAt && row.createdAt.slice(0, 10),
-			sortable: true,
-		},
-		{
-			id: 'details',
-			name: 'Select',
-			cell: (row) => (
-				<Button
-					className="font-normal border border-[#F4743B] hover:bg-[#F4743B] hover:text-white rounded-lg p-1 dark:text-white"
-					type="button"
-					text="Details"
-					onClick={() => handleDetailsClick(row)}
-				/>
-			),
-		},
-		{
-			id: 'edit',
-			cell: (row) =>
-				selectedRequest?.id === row.id ? (
-					<i className="fas fa-edit mr-1 text-lg"></i>
-				) : (
-					// No typo here, it's a Button component
-					<Button
-						className="font-normal border border-[#F4743B] hover:bg-[#F4743B] hover:text-white rounded-lg p-1 dark:text-white"
-						type="button"
-						text="Cancel"
-					/>
-				),
-		},
-		{
-			id: 'completed',
-			cell: (row) =>
-				selectedRequest?.id !== row.id && (
-					<Button
-						className="font-normal border border-green-500 hover:bg-green-300 rounded-lg p-1 dark:text-white"
-						type="button"
-						text="Completed"
-					/>
-				),
-		},
-	];
+export default function requestColumns({ handleDetailsClick, user }) {
+  return [
+    {
+      id: "title",
+      name: "Title",
+      selector: (row) => row.title,
+    },
+    {
+      id: "status",
+      name: "Status",
+      selector: (row) => (row.isActive ? "Active" : "Inactive"),
+      sortable: true,
+    },
+    {
+      id: "city",
+      name: "City",
+      selector: (row) => row.location.city,
+      sortable: true,
+    },
+    {
+      id: "country",
+      name: "Country",
+      selector: (row) => row.location.country,
+      sortable: true,
+    },
+    {
+      id: "date",
+      name: "Date",
+      selector: (row) => new Date(row.createdAt).toLocaleDateString(),
+      sortable: true,
+    },
+    {
+      id: "details",
+      name: "Select",
+      button: true,
+      cell: (row) => (
+        <Button
+          className="font-normal border border-[#F4743B] hover:bg-[#F4743B] hover:text-white rounded-lg p-1 dark:text-white"
+          type="button"
+          text="Details"
+          onClick={() => handleDetailsClick(row)}
+        />
+      ),
+    },
+    {
+      id: "cancel",
+      button: true,
+      cell: (row) =>
+        row.isAccepted && row.beekeeper === user.email ? (
+          <Button
+            className="font-normal border border-[#F4743B] hover:bg-[#F4743B] hover:text-white rounded-lg p-1 dark:text-white"
+            type="button"
+            text="Cancel"
+          />
+        ) : null,
+    },
+    // For now will hide the 'completed' button until the 'completed' feature is added later on, this can be enabled in the future
+    // {
+    // 	id: 'completed',
+    // 	cell: (row) =>
+    // 		selectedRequest?.id !== row.id && (
+    // 			<Button
+    // 				className="font-normal border border-green-500 hover:bg-green-300 rounded-lg p-1 dark:text-white"
+    // 				type="button"
+    // 				text="Completed"
+    // 			/>
+    // 		),
+    // },
+  ];
 }
+
+// Ensure PropTypes are defined for the component
+requestColumns.propTypes = {
+  handleDetailsClick: PropTypes.func.isRequired,
+  selectedRequest: PropTypes.object,
+  user: PropTypes.object.isRequired,
+};
