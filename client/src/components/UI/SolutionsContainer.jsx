074989ac48b816ebe1bf735ccd630871c80df7df
Nisha <nishavijai@gmail.com>
2024-10-03 16:56:06 +0200

    Add orange button on details summary components
    Add orange button (svg) image and update design (follow Figma design) on relevant components.

--- client/src/components/UI/SolutionsContainer.jsx
+++ client/src/components/UI/SolutionsContainer.jsx
@@ -2,41 +2,46 @@ import { DetailsSummary } from "./DetailsSummary";
 import beekeepersImage from "../../assets/imgs/beekeepers.png";
 
 export const SolutionsContainer = () => {
-  const customStyles = "pt-8 pr-8 pl-8 text-[#101828] text-18px font-semibold";
+  const customStyles = "text-[#101828] text-18px font-semibold";
 
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col pt-6 md:pt-14 md:min-h-[100%]">
       <section className="flex flex-col justify-between gap-4 md:flex-row">
-        <section className="bg-[#D8E5C3] dark:bg-[#3D4D22] h-72 md:w-1/2 rounded-xl">
-          <DetailsSummary
-            className={customStyles}
-            title="Plant Bee-Friendly Flowers"
-            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-          />
-          <DetailsSummary
-            className={customStyles}
-            title="Avoid Peticides"
-            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-          />
-          {/* <DetailsSummary className={customStyles} title="Provide Water" description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning." /> */}
+        <section className="bg-[#D8E5C3] rounded-xl md:w-5/12 min-h-full ">
+          <section className="px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 dark:bg-[#3D4D22]">
+            <DetailsSummary open
+              className={customStyles}
+              title="Plant Bee-Friendly Flowers"
+              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+            />
+            <DetailsSummary
+              className={customStyles}
+              title="Avoid Peticides"
+              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+            />
+            <DetailsSummary className={customStyles} title="Create Habitats" description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food." />
+          </section>
         </section>
 
-        <section className="bg-[#D8E5C3] dark:bg-[#3D4D22] h-72 md:w-1/2 rounded-xl">
-          <DetailsSummary
-            className={customStyles}
-            title="Create a Bee Bath"
-            description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning."
-          />
-          <DetailsSummary
-            className={customStyles}
-            title="Create Habitats"
-            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-          />
-          {/* <DetailsSummary className={customStyles} title="Support Local Beekeepers" description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning." /> */}
+
+        <section className="bg-[#D8E5C3] rounded-xl md:w-5/12 min-h-full">
+          <section className="px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 dark:bg-[#3D4D22]">
+            <DetailsSummary open
+              className={customStyles}
+              title="Provide Water"
+              description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning."
+            />
+            <DetailsSummary
+              className={customStyles}
+              title="Support Local Beekeepers"
+              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+            />
+            <DetailsSummary className={customStyles} title="Create a Bee Bath" description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning." />
+          </section>
         </section>
 
-        <section className="grid justify-items-center">
-          <img src={beekeepersImage} alt="beekeepers" />
+        <section className="md:w-3/12 min-h-full">
+          <img src={beekeepersImage} className="w-[100%] h-[100%]" alt="beekeepers" />
         </section>
       </section>
     </section>
