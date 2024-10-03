64b339689b7d56af07eaf117105dc516c6749b6b
Nisha <nishavijai@gmail.com>
2024-10-03 17:24:25 +0200

    Update dark mode text color change on relevant components

--- client/src/components/UI/SolutionsContainer.jsx
+++ client/src/components/UI/SolutionsContainer.jsx
@@ -7,37 +7,32 @@ export const SolutionsContainer = () => {
   return (
     <section className="flex flex-col pt-6 md:pt-14 md:min-h-[100%]">
       <section className="flex flex-col justify-between gap-4 md:flex-row">
-        <section className="bg-[#D8E5C3] rounded-xl md:w-5/12 min-h-full ">
-          <section className="px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 dark:bg-[#3D4D22]">
-            <DetailsSummary open
-              className={customStyles}
-              title="Plant Bee-Friendly Flowers"
-              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-            />
-            <DetailsSummary
-              className={customStyles}
-              title="Avoid Peticides"
-              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-            />
-            <DetailsSummary className={customStyles} title="Create Habitats" description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food." />
-          </section>
+        <section className="bg-[#D8E5C3] rounded-xl min-h-full px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 md:w-5/12 dark:bg-[#3D4D22]">
+          <DetailsSummary open
+            className={customStyles}
+            title="Plant Bee-Friendly Flowers"
+            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+          />
+          <DetailsSummary
+            className={customStyles}
+            title="Avoid Peticides"
+            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+          />
+          <DetailsSummary className={customStyles} title="Create Habitats" description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food." />
         </section>
 
-
-        <section className="bg-[#D8E5C3] rounded-xl md:w-5/12 min-h-full">
-          <section className="px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 dark:bg-[#3D4D22]">
-            <DetailsSummary open
-              className={customStyles}
-              title="Provide Water"
-              description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning."
-            />
-            <DetailsSummary
-              className={customStyles}
-              title="Support Local Beekeepers"
-              description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
-            />
-            <DetailsSummary className={customStyles} title="Create a Bee Bath" description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning." />
-          </section>
+        <section className="bg-[#D8E5C3] rounded-xl min-h-full px-3 py-6 flex flex-col gap-6 md:py-10 md:justify-stretch md:gap-8 md:w-5/12 dark:bg-[#3D4D22]">
+          <DetailsSummary open
+            className={customStyles}
+            title="Provide Water"
+            description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning."
+          />
+          <DetailsSummary
+            className={customStyles}
+            title="Support Local Beekeepers"
+            description="Grow a variety of plants that flower at different times of the year to provide bees with a constant source of food."
+          />
+          <DetailsSummary className={customStyles} title="Create a Bee Bath" description="Leave a shallow water source with landing spots, like pebbles or twigs, so bees can hydrate without drowning." />
         </section>
 
         <section className="md:w-3/12 min-h-full">
