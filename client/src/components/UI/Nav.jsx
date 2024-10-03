aa6dc6522c3a557fae26b8eb7e317bc7cf9a45e9
Nisha <nishavijai@gmail.com>
2024-10-03 17:54:49 +0200

    Fix dark mode toggle issue on small screens

--- client/src/components/UI/Nav.jsx
+++ client/src/components/UI/Nav.jsx
@@ -11,12 +11,7 @@ export const Nav = ({ action, onClickHandler }) => {
   const burgerMenuContainer = document.querySelector(".burger-menu-container");
 
   const onChangeToggleClassHandler = () => {
-    setIsNavMenuOpen(true);
-    burgerMenuContainer?.classList.toggle("change");
-  };
-
-  const onClickCloseNavBarMenuHandler = () => {
-    setIsNavMenuOpen(false);
+    setIsNavMenuOpen(!isNavMenuOpen);
     burgerMenuContainer?.classList.toggle("change");
   };
 
@@ -28,7 +23,7 @@ export const Nav = ({ action, onClickHandler }) => {
           className={`${isNavMenuOpen
             ? "flex flex-col items-center justify-center gap-6 absolute top-0 left-0 w-full h-dvh bg-neutral-500 z-40"
             : "flex flex-col space-y-6 p-4 md:p-0 mt-14 md:flex-row md:items-center md:space-x-8 md:space-y-0"
-            }`} onClick={() => onClickCloseNavBarMenuHandler()}>
+            }`}>
           <li>
             <AnchorLink
               href="#"
