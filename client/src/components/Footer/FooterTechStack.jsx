d7db3565558228360f99de385ed2400a732adca6
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-10-07 17:31:17 +1300

    updated tailwind styling to make two columns

--- client/src/components/Footer/FooterTechStack.jsx
+++ client/src/components/Footer/FooterTechStack.jsx
@@ -13,20 +13,27 @@ function FooterTechStack({ techstack }) {
         Tech Stack
       </h2>
 
-      {techstack.map((tech, index) => (
-        <div key={index} className="flex justify-between items-center w-full px-4 object-contain">
-          <a
-            href="#"
-            className="group"
-            onClick={(e) => handleClick(e, tech.url)}>
-            <img
-              src={tech.image}
-              alt={tech.name}
-              className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
-            />
-          </a>
-        </div>
-      ))}
+      <div className="grid grid-cols-2 gap-4 w-full">
+    {techstack.map((tech, index) => (
+      <div
+        key={index}
+        className={`flex justify-between items-center px-4 object-contain ${
+          index === techstack.length - 1 ? 'col-span-2' : ''
+        }`}
+      >
+        <a
+          href="#"
+          className="group"
+          onClick={(e) => handleClick(e, tech.url)}>
+          <img
+            src={tech.image}
+            alt={tech.name}
+            className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
+          />
+        </a>
+      </div>
+    ))}
+  </div>
       {modalIndex && (
         <ModalResourceLink url={modalIndex} handleClick={setModalIndex} />
       )}
