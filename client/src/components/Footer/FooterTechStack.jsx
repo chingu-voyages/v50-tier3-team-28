3650c20002812325411cf60c5adb423c0370d6f0
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-10-07 15:56:45 +1300

    added link to javascript icon

--- client/src/components/Footer/FooterTechStack.jsx
+++ client/src/components/Footer/FooterTechStack.jsx
@@ -1,3 +1,4 @@
+import ModalResourceLink from "./ModalResourceLink";
 import jsLogo from "../../assets/images/footer/js_logo.png";
 import reactLogo from "../../assets/images/footer/react_logo.png";
 import nodeLogo from "../../assets/images/footer/node_js_logo.png";
@@ -5,17 +6,26 @@ import mongoDBLogo from "../../assets/images/footer/mongo_db_logo.png";
 import tailwindLogo from "../../assets/images/footer/tailwindcss_logo.png";
 
 function FooterTechStack() {
+  const [modalIndex, setModalIndex] = useState(null);
+  const handleClick = (index) => {
+    setModalIndex(index);
+  };
   return (
     <div className="box2 px-4 md:px-8 w-full lg:w-1/3 border border-footerBoxColor flex flex-col items-center rounded-3xl">
       <h2 className="text-xl font-bold my-4 text-footerProfileNameColor dark:text-white">
         Tech Stack
       </h2>
       <div className="flex justify-between items-center w-full px-4 object-contain">
-        <img
-          src={jsLogo}
-          alt="JavaScript"
-          className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
-        />
+        <a onClick={()=> handleClick(index)}>
+          <img
+            src={jsLogo}
+            alt="JavaScript"
+            className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
+          />
+        </a>
+        {modalIndex === index && (
+              <ModalResourceLink url={"https://developer.mozilla.org/en-US/docs/Web/JavaScript"} handleClick={handleClick} />
+            )}
         <img
           src={reactLogo}
           alt="React"
