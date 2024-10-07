2633621ee7cd9f148f2e6a573bd32a226a2b3039
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-10-07 17:15:10 +1300

    created techstackdata and mapped data in footertechstack. Also added/used ModalResourceLink component

--- client/src/components/Footer/FooterTechStack.jsx
+++ client/src/components/Footer/FooterTechStack.jsx
@@ -1,56 +1,35 @@
 import ModalResourceLink from "./ModalResourceLink";
-import jsLogo from "../../assets/images/footer/js_logo.png";
-import reactLogo from "../../assets/images/footer/react_logo.png";
-import nodeLogo from "../../assets/images/footer/node_js_logo.png";
-import mongoDBLogo from "../../assets/images/footer/mongo_db_logo.png";
-import tailwindLogo from "../../assets/images/footer/tailwindcss_logo.png";
+import { useState } from "react";
 
-function FooterTechStack() {
+function FooterTechStack({ techstack }) {
   const [modalIndex, setModalIndex] = useState(null);
-  const handleClick = (index) => {
-    setModalIndex(index);
+  const handleClick = (e, url) => {
+    e.preventDefault();
+    setModalIndex(url);
   };
   return (
     <div className="box2 px-4 md:px-8 w-full lg:w-1/3 border border-footerBoxColor flex flex-col items-center rounded-3xl">
       <h2 className="text-xl font-bold my-4 text-footerProfileNameColor dark:text-white">
         Tech Stack
       </h2>
-      <div className="flex justify-between items-center w-full px-4 object-contain">
-        <a onClick={()=> handleClick(index)}>
-          <img
-            src={jsLogo}
-            alt="JavaScript"
-            className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
-          />
-        </a>
-        {modalIndex === index && (
-              <ModalResourceLink url={"https://developer.mozilla.org/en-US/docs/Web/JavaScript"} handleClick={handleClick} />
-            )}
-        <img
-          src={reactLogo}
-          alt="React"
-          className="w-24 xl:w-32 h-24 xl:h-32 object-contain ml-2 xl:ml-4"
-        />
-      </div>
-      <div className="flex justify-between items-center w-full px-4 mt-4 xl:mt-10 object-contain">
-        <img
-          src={nodeLogo}
-          alt="Node.js"
-          className="w-24 xl:w-32 h-24 lg:h-32 object-contain mr-2 xl:mr-4"
-        />
-        <img
-          src={mongoDBLogo}
-          alt="MongoDB"
-          className="w-24 xl:w-32 h-24 xl:h-32 object-contain ml-2 xl:ml-4"
-        />
-      </div>
-      <div className="flex justify-center items-center w-full mt-10 xl:mt-20">
-        <img
-          src={tailwindLogo}
-          alt="Tailwind CSS"
-          className="w-40 xl:w-80 h-10 object-contain mt-2 mb-6"
-        />
-      </div>
+
+      {techstack.map((tech, index) => (
+        <div key={index} className="flex justify-between items-center w-full px-4 object-contain">
+          <a
+            href="#"
+            className="group"
+            onClick={(e) => handleClick(e, tech.url)}>
+            <img
+              src={tech.image}
+              alt={tech.name}
+              className="w-24 xl:w-32 h-24 xl:h-32 object-contain mr-2 xl:mr-4"
+            />
+          </a>
+        </div>
+      ))}
+      {modalIndex && (
+        <ModalResourceLink url={modalIndex} handleClick={setModalIndex} />
+      )}
     </div>
   );
 }
---
+++ client/src/components/Footer/FooterTechStackData.js
@@ -0,0 +1,33 @@
+import jsLogo from "../../assets/images/footer/js_logo.png";
+import reactLogo from "../../assets/images/footer/react_logo.png";
+import nodejsLogo from "../../assets/images/footer/node_js_logo.png";
+import mongodbLogo from "../../assets/images/footer/mongo_db_logo.png";
+import tailwindcssLogo from "../../assets/images/footer/tailwindcss_logo.png";
+
+export const techstack = [
+    {
+        name: "JavaScript",
+        image: jsLogo,
+        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    {
+        name: "React",
+        image: reactLogo,
+        url: "https://react.dev/",
+    },
+    {
+        name: "NodeJS",
+        image: nodejsLogo,
+        url: "https://nodejs.org/",
+    },
+    {
+        name: "MongoDB",
+        image: mongodbLogo,
+        url: "https://www.mongodb.com/",
+    },
+    {
+        name: "tailwindcss",
+        image: tailwindcssLogo,
+        url: "https://tailwindcss.com/",
+    },
+];
