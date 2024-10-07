6316d8ebd2ced89437ca7948ff1bfe475c8f779a
ss-deep <deepalisangole@gmail.com>
2024-10-06 23:09:33 -0500

    added fetch function to show updated request data

--- client/src/components/UserProfile/RequestTable.jsx
+++ client/src/components/UserProfile/RequestTable.jsx
@@ -10,96 +10,98 @@ import columns from './requestTableColumns';
 // import CancelRequest from '../UI/CancelRequest';
 
 const tableCustomStyles = {
-  headRow: {
-    style: {
-      backgroundColor: '#f0f0f2',
-    },
-  },
+	headRow: {
+		style: {
+			backgroundColor: '#f0f0f2',
+		},
+	},
 };
 
 export const RequestComponent = ({ fixedHeader, fixedHeaderScrollHeight }) => {
-  const { getAccessTokenSilently, user } = useAuth0();
-  const [requestData, setRequestData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedRequest, setSelectedRequest] = useState(null);
+	const { getAccessTokenSilently, user } = useAuth0();
+	const [requestData, setRequestData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [showModal, setShowModal] = useState(false);
+	const [selectedRequest, setSelectedRequest] = useState(null);
 
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  const apiUrl = isDevelopment
-    ? 'http://localhost:3003/api'
-    : 'https://be-v50-tier3-team-28.onrender.com/api';
+	const isDevelopment = process.env.NODE_ENV === 'development';
+	const apiUrl = isDevelopment
+		? 'http://localhost:3003/api'
+		: 'https://be-v50-tier3-team-28.onrender.com/api';
 
-  const fetchUserData = useCallback(async () => {
-    try {
-      const accessToken = await getAccessTokenSilently();
-      const response = await axios.get(`${apiUrl}/requests`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const data = response.data.requests;
-      setRequestData(data);
-      setLoading(false);
-    } catch (e) {
-      console.error(e);
-      setLoading(false);
-    }
-  }, [getAccessTokenSilently, apiUrl]);
+	const fetchUserData = useCallback(async () => {
+		try {
+			const accessToken = await getAccessTokenSilently();
+			const response = await axios.get(`${apiUrl}/requests`, {
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+			});
+			const data = response.data.requests;
+			setRequestData(data);
+			setLoading(false);
+		} catch (e) {
+			console.error(e);
+			setLoading(false);
+		}
+	}, [getAccessTokenSilently, apiUrl]);
 
-  useEffect(() => {
-    fetchUserData();
-  }, [fetchUserData]);
+	useEffect(() => {
+		fetchUserData();
+	}, [fetchUserData]);
 
-  const handleDetailsClick = (row) => {
-    setSelectedRequest(row);
-    setShowModal(true);
-  };
+	const handleDetailsClick = (row) => {
+		setSelectedRequest(row);
+		setShowModal(true);
+		fetchUserData();
+	};
 
-  const handleModalClose = () => {
-    setShowModal(false);
-    setSelectedRequest(null);
-  };
+	const handleModalClose = () => {
+		setShowModal(false);
+		setSelectedRequest(null);
+		fetchUserData();
+	};
 
-  const handleRequestAcceptance = (id) => {
-    setRequestData((prevData) =>
-      prevData.map((request) =>
-        request.id === id
-          ? { ...request, isAccepted: true, beekeeper: user.email }
-          : request
-      )
-    );
-    fetchUserData(); // Fetch the latest data after accepting a request
-  };
+	const handleRequestAcceptance = (id) => {
+		setRequestData((prevData) =>
+			prevData.map((request) =>
+				request.id === id
+					? { ...request, isAccepted: true, beekeeper: user.email }
+					: request
+			)
+		);
+		fetchUserData(); // Fetch the latest data after accepting a request
+	};
 
-  return (
-    <>
-      <DataTable
-        columns={columns({
-          handleDetailsClick,
-          selectedRequest,
-          user,
-        })}
-        data={requestData}
-        fixedHeader={fixedHeader}
-        // fixedHeaderScrollHeight={fixedHeaderScrollHeight}
-        highlightOnHover
-        customStyles={tableCustomStyles}
-        pagination
-        progressPending={loading}
-      />
+	return (
+		<>
+			<DataTable
+				columns={columns({
+					handleDetailsClick,
+					selectedRequest,
+					user,
+				})}
+				data={requestData}
+				fixedHeader={fixedHeader}
+				// fixedHeaderScrollHeight={fixedHeaderScrollHeight}
+				highlightOnHover
+				customStyles={tableCustomStyles}
+				pagination
+				progressPending={loading}
+			/>
 
-      {showModal && selectedRequest && (
-        <AcceptRequestModal
-          key={selectedRequest._id}
-          request={selectedRequest}
-          onClose={handleModalClose}
-          onAccept={() => handleRequestAcceptance(selectedRequest.id)}
-        />
-      )}
-    </>
-  );
+			{showModal && selectedRequest && (
+				<AcceptRequestModal
+					key={selectedRequest._id}
+					request={selectedRequest}
+					onClose={handleModalClose}
+					onAccept={() => handleRequestAcceptance(selectedRequest.id)}
+				/>
+			)}
+		</>
+	);
 };
 RequestComponent.propTypes = {
-  fixedHeader: PropTypes.bool,
-  fixedHeaderScrollHeight: PropTypes.string,
+	fixedHeader: PropTypes.bool,
+	fixedHeaderScrollHeight: PropTypes.string,
 };
