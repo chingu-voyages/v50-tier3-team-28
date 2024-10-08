93d82491fb28fc8a32c58ca7ceb3c4781e188811
Kseniia Riabova <kseniaistratova@gmail.com>
2024-10-08 13:47:17 +0200

    "feat: Add endpoint to count user requests as beefinder and completed requests as beekeeper
    - Implemented getUserRequestCounts function to count requests where the user is a beefinder.
    - Added logic to count completed requests where the user is a beekeeper.- Created a new route to retrieve these counts.
    - Tested the endpoint with Postman.
    - Updated API documentation with a successful result example."
    
    Co-authored-by: Valeriusdev <Valeriusdev@users.noreply.github.com>

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   updateOrDeleteMetadata,
   deleteUserfromDb,
+  getUserRequestCounts,
 } = require('../controllers/userController');
 const router = express.Router();
 const { jwtCheck } = require('../middleware/jwtMiddleware');
 
 router.patch('/metadata', jwtCheck, updateOrDeleteMetadata);
 router.delete('/:userId', jwtCheck, deleteUserfromDb);
+router.get('/request-counts', jwtCheck, getUserRequestCounts);
 
 module.exports = router;
