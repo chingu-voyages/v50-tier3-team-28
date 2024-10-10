ba3f01d0dc9de8acdb3df8bc747e19c20077a221
r1g023 <limon1285@gmail.com>
2024-10-10 00:09:30 -0500

    fixed map that was overlapping request Modal, also replaced axios with Fetch on AcceptRequestCall

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,9 +1,9 @@
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
-import mapData from "./mapData";
-import greenBeeIconUrl from "../assets/icons/green-bee.png";
-import redBeeIconUrl from "../assets/icons/red-bee.png";
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import mapData from './mapData';
+import greenBeeIconUrl from '../assets/icons/green-bee.png';
+import redBeeIconUrl from '../assets/icons/red-bee.png';
 
 const greenBeeIcon = new Icon({
   iconUrl: greenBeeIconUrl,
@@ -18,11 +18,11 @@ const redBeeIcon = new Icon({
 export const Map = () => {
   return (
     <MapContainer
-      className="leaflet-container w-[150rem] h-[25rem]"
+      className="leaflet-container w-[150rem] h-[25rem] z-0"
       center={[0, 0]}
       zoom={2}
       minZoom={2}
-    // scrollWheelZoom={true}
+      // scrollWheelZoom={true}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -32,20 +32,29 @@ export const Map = () => {
         <Marker
           key={marker.id}
           position={[marker.coordinates.latitude, marker.coordinates.longitude]}
-          icon={marker.status ? greenBeeIcon : redBeeIcon}
-        >
+          icon={marker.status ? greenBeeIcon : redBeeIcon}>
           <Popup>
             <div>
               <h2 className="text-center underline mb-1"> {marker.country} </h2>
-              <div> <span className="underline">City</span>: {marker.city} </div>
-              <div> <span className="underline">Latitude</span>: {marker.coordinates.latitude} </div>
-              <div> <span className="underline">Longitude</span>: {marker.coordinates.longitude} </div>
-              <div> <span className="underline">Status</span>: {marker.status ? "Saved" : "Found"} </div>
+              <div>
+                <span className="underline">City</span>: {marker.city}
+              </div>
+              <div>
+                <span className="underline">Latitude</span>:
+                {marker.coordinates.latitude}
+              </div>
+              <div>
+                <span className="underline">Longitude</span>:
+                {marker.coordinates.longitude}
+              </div>
+              <div>
+                <span className="underline">Status</span>:
+                {marker.status ? 'Saved' : 'Found'}
+              </div>
             </div>
           </Popup>
         </Marker>
-      ))
-      }
+      ))}
     </MapContainer>
   );
 };
