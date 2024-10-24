013aa80f1dd7be0795f27868ea8892d2f85ef29b
Kseniia Riabova <kseniaistratova@gmail.com>
2024-10-24 19:00:18 +0200

    Reduce the amount of unnecessary requests if DB is already connected

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log('Database connected!'))
-    .catch((err) => console.log(err));
+const connection = {};
+
+const connectDB = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log('Using Existing Connection');
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    connection.isConnected = db.connections[0].readyState;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error connecting to database');
+  }
 };
 module.exports = connectDB;
